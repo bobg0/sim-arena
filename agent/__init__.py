e8e6843ec967d51df3767b8e52c61885832710a3
RuiZhangg <zr_rachel04@outlook.com>
2026-02-24 11:35:58 -0800

    implemented random agent

--- agent/__init__.py
+++ agent/__init__.py
@@ -2,13 +2,14 @@
 Agent module for reinforcement learning.
 """
 
-
 from .eps_greedy import EpsilonGreedyAgent
 from .dqn import DQNAgent
+from .random import RandomAgent
 
 __all__ = [
     'Agent',
     'AgentType',
     'EpsilonGreedyAgent',
-    'DQNAgent'
+    'DQNAgent',
+    'RandomAgent'
 ]
